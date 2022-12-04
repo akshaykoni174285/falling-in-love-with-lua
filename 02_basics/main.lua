4fd3409f403b1a5145a340da228d4e81547bf4ff
akshaykoni174285 <akshaykoni174285@gmail.com>
2022-12-05 02:35:29 +0530

    5 dec

--- 02_basics/main.lua
+++ 02_basics/main.lua
@@ -3,6 +3,9 @@
 function love.load()
     local anim8 = require '/libraries/anim8'
     love.graphics.setDefaultFilter("nearest","nearest")
+
+    local sti = require "/libraries/sti"
+    gamemap = sti("/maps/test_map.lua")
     player = {}
     player.x = 200
     player.y = 200
@@ -61,10 +64,11 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.draw(background)
+    gamemap:draw()
     -- love.graphics.setColor(255,255,255)
     -- love.graphics.draw(player.sprites,player.x,player.y)
     -- love.graphics.circle("fill",player.x,player.y,player.radius)
     -- love.graphics.setColor(255,255,255)
-    player.anim:draw(player.spritesheet,player.x,player.y,nil,10)
+    player.anim:draw(player.spritesheet,player.x,player.y,nil,5)
+    
 end
