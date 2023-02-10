2b3528263cf29bad5eb643c8c1d4896eb31ff9b9
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-02-10 13:41:40 +0530

    10 feb

--- 01_basics/main.lua
+++ 01_basics/main.lua
@@ -1,32 +1,69 @@
 
+-- function love.load()
+--     player = {}
+--     player.x = 200
+--     player.y = 200
+--     player.speed = 3
+--     player.sprites = love.graphics.newImage("/sprites/parrot.png")
+--     background = love.graphics.newImage("sprites/maxresdefault (1).jpg")
+-- end
+
+-- function love.update(dt)
+--     -- player.x = player.x + 1
+--     if love.keyboard.isDown("right") then
+--         player.x = player.x + player.speed
+--     end
+--     if love.keyboard.isDown("left") then
+--         player.x = player.x - player.speed
+--     end
+--     if love.keyboard.isDown("up") then
+--         player.y = player.y - player.speed
+--     end
+--     if love.keyboard.isDown("down") then
+--         player.y = player.y + player.speed
+--     end
+
+-- end
+
+-- function love.draw()
+--     love.graphics.draw(background)
+--     love.graphics.draw(player.sprites,player.x,player.y)
+--     -- love.graphics.setColor(255,255,255)
+-- end
+
+
+
 function love.load()
+    -- all the initialization 
     player = {}
-    player.x = 200
-    player.y = 200
+    profile = love.graphics.newImage("sprites/parrot.png")
+    background = love.graphics.newImage("sprites/background.png")
+    player.x=love.graphics.getWidth()/2 - profile:getWidth()/2
+    player.y=love.graphics.getHeight()/2 - profile:getHeight()/2
     player.speed = 3
-    player.sprites = love.graphics.newImage("/sprites/parrot.png")
-    background = love.graphics.newImage("sprites/maxresdefault (1).jpg")
+
 end
 
-function love.update(dt)
-    -- player.x = player.x + 1
-    if love.keyboard.isDown("right") then
-        player.x = player.x + player.speed
-    end
-    if love.keyboard.isDown("left") then
-        player.x = player.x - player.speed
-    end
+function love.update(dt) 
     if love.keyboard.isDown("up") then
         player.y = player.y - player.speed
     end
     if love.keyboard.isDown("down") then
         player.y = player.y + player.speed
     end
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed
+    end
+    if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed
+    end
+
+
 
 end
 
 function love.draw()
     love.graphics.draw(background)
-    love.graphics.draw(player.sprites,player.x,player.y)
-    -- love.graphics.setColor(255,255,255)
+    love.graphics.draw(profile,player.x,player.y)
+    
 end
